ae91bcbc071392b90a2dcf203dff676ce2174c64
Your Name <you@example.com>
2024-04-07 22:26:21 +0800

    raw to yuyv/uyvy draft

--- Makefile
+++ Makefile
@@ -7,7 +7,18 @@
 dcrawcvt: dcrawcvt.o
 #	gcc -g -O2 -o $@ $<
 
-test: dcrawcvt
-	./dcrawcvt -g 1920x1080 ../1920_1080_raw8_r12_0x1fa400.bin ../a.rgb
+torgb: clean dcrawcvt
+	./dcrawcvt -g 1920x1080 ../1920_1080_raw8_r12_0x1fa400.bin a.rgb
+	convert -depth 8 -size 1920x1080+0 rgb:a.rgb a.png
+	open a.png
 
+toyuv: clean dcrawcvt
+	./dcrawcvt -g 1920x1080 ../1920_1080_raw8_r12_0x1fa400.bin a.yuv
+	convert  -colorspace YCbCr -sampling-factor 4:2:2 -size 1920x1080+0 uyvy:a.yuv a.png
+	open a.png
+
+clean:
+	-rm a.yuv
+	-rm a.rgb
+	-rm a.png
 .PHONY: test
--- dcrawcvt.c
+++ dcrawcvt.c
@@ -11,7 +11,7 @@ char * read_file(char *path, size_t *rsize)
     long size = ftell(fd);
     fseek(fd, 0, SEEK_SET);
     printf("path %s size %ld\n", path, size);
-    char *buf = malloc(size);
+    char *buf = calloc(1, size);
     ssize_t nread = fread(buf, size, 1, fd);
     if (nread != 1) {
         printf("read error %lld\n", nread);
@@ -44,19 +44,86 @@ void dcraw_to_rgb(char *buf, int w, int h, char *rgb_buf)
     }
 }
 
+void dcraw_to_uyvy(unsigned char *buf, int w, int h, char *yuv_buf)
+{
+    float sCb = 128, sCr = 128;
+    for(int row = 2; row < h-2; row++) {
+        for (int col = 2; col < w-2; col++) {
+            int off = (w*row + col)*2;
+            float r = (RAW(row,col) + RAW(row,col+2) + RAW(row+2,col) + RAW(row+2,col+2))>>2;
+            float g = (RAW(row,col+1) + RAW(row+1,col) + RAW(row+1,col+2) + RAW(row+2,col+1))>>2;
+            float b = (RAW(row-1,col-1) + RAW(row-1,col+1) + RAW(row+1,col-1) + RAW(row+1,col+1))>>2;
+            //ITU-R BT.709
+            float y = 16 + 0.183*r + 0.614*g + 0.062*b;
+            float cb = 128 - 0.101*r - 0.339*g + 0.439*b;
+            float cr = 128 + 0.439*r - 0.399*g - 0.040*b;
+            if (off&1 == 0) {
+                yuv_buf[off+1] = y;
+                yuv_buf[off] = sCb = cb;
+                yuv_buf[off+2] = sCr = cr;
+            } else {
+                yuv_buf[off+1] = y;
+                yuv_buf[off-2] = (sCb + cb)/2;
+                yuv_buf[off] = (sCr + cr)/2;
+            }
+        }
+    }
+}
+
+void dcraw_to_yuyv(unsigned char *buf, int w, int h, char *yuv_buf)
+{
+    float sCb = 128, sCr = 128;
+    for(int row = 2; row < h-2; row++) {
+        for (int col = 2; col < w-2; col++) {
+            int off = (w*row + col)*2;
+            float r = (RAW(row,col) + RAW(row,col+2) + RAW(row+2,col) + RAW(row+2,col+2))/4;
+            float g = (RAW(row,col+1) + RAW(row+1,col) + RAW(row+1,col+2) + RAW(row+2,col+1))/4;
+            float b = (RAW(row-1,col-1) + RAW(row-1,col+1) + RAW(row+1,col-1) + RAW(row+1,col+1))/4;
+            // printf("off %d r %f g %f b %f\n", off, r, g, b);
+            // exit(0);
+            //ITU-R BT.709
+            float y = 16 + 0.183*r + 0.614*g + 0.062*b;
+            float cb = 128 - 0.101*r - 0.339*g + 0.439*b;
+            float cr = 128 + 0.439*r - 0.399*g - 0.040*b;
+            // printf("off %d y %f cb %f cr %f\n", off, y, cb, cr);
+
+            if (off&1 == 0) {
+                yuv_buf[off] = y;
+                yuv_buf[off+1] = sCb = cb;
+                yuv_buf[off+3] = sCr = cr;
+            } else {
+                yuv_buf[off] = y;
+                yuv_buf[off-1] = (sCb + cb)/2;
+                yuv_buf[off+1] = (sCr + cr)/2;
+            }
+            // if (off >10000)
+            // exit(0);
+        }
+    }
+}
+
 // TODO: char * or unsigned char*?
 void dcraw_to_yuv(int debayer_pattern, int w, int h, char *infile, char *outfile)
 {
     size_t insize = 0;
     char *buf = read_file(infile, &insize);
-    char *rgb_buf = malloc(insize * 3);
     if (insize < w*h) {
         printf("input file is truncated? intput file size %ld wxh %ld\n", insize, w*h);
         exit(EXIT_FAILURE);
     }
+#ifdef TO_RGB
+    char *rgb_buf = malloc(insize * 3);
+    memset(rgb_buf, 0xff, insize*3);
+
     dcraw_to_rgb(buf, w, h, rgb_buf);
     write_file(outfile, rgb_buf, insize * 3);
-    // rgb_to_yuv(rgb_buf, yuv_buf);
+#else
+    char *yuv_buf = malloc(insize * 2);
+    memset(yuv_buf, 0, insize*2);
+
+    dcraw_to_yuyv(buf, w, h, yuv_buf);
+    write_file(outfile, yuv_buf, insize * 2);
+#endif
 }
 
 void parse_geometry(char *gstr, int *w, int *h)
